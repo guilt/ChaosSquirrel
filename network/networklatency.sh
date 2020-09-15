ef39ceb791ba935f6a55559396ad701f0b8b3df5
Karthik Kumar Viswanathan <karthikkumar@gmail.com>
2020-09-15 06:06:33 -0700

    Fix Latency Script.

--- network/networklatency.sh
+++ network/networklatency.sh
@@ -3,7 +3,7 @@
 
 # Adds 1000ms +- 500ms of latency to each packet
 ip -o link show | awk -F': ' '{print $2}' | while read -r IF; do
-	tc qdisc add dev "${IF}" root latency delay 1000ms 500ms &
+	tc qdisc add dev "${IF}" root netem delay 1000ms 500ms &
 done
 
 wait
