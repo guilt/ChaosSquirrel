4cab370f05596f5fca293610dcb92e6d136d79c2
Karthik Kumar Viswanathan <karthikkumar@gmail.com>
2020-09-15 00:11:06 -0700

    More Fixes to Dsc Generation.

--- .build/deb/buildDeb.sh
+++ .build/deb/buildDeb.sh
@@ -3,8 +3,22 @@
 SCRIPT=$(realpath "${BASH_SOURCE[0]}")
 SCRIPT_DIR=$( cd "$( dirname "${SCRIPT}" )" >/dev/null 2>&1 && pwd )
 
+VERSION=$(git describe --tags HEAD)
+TAR_FILE=$("${SCRIPT_DIR}/../tar/buildTar.sh")
 RPM_FILE=$("${SCRIPT_DIR}/../rpm/buildRpm.sh")
-DEB_FILE=$(sudo alien -d --scripts "${RPM_FILE}" | grep -F generated | awk '{print $1}')
+DEBDATE=$(date -R)
+SIZE=$(ls -la "${TAR_FILE}" | awk '{print $5}')
+MD5=$(openssl md5 "${TAR_FILE}" | awk '{print $2}')
 
+cd ${SCRIPT_DIR} || exit 1
+for TFILE in ChaosSquirrel.dsc debian.changelog; do
+    cat "${TFILE}.in" \
+        | sed "s/@VERSION@/$VERSION/g" \
+        | sed "s/@DEBDATE@/$DEBDATE/g" \
+        | sed "s/@MD5@/$MD5/g" \
+        > "${TFILE}"
+done
+
+DEB_FILE=$(sudo alien -d --scripts "${RPM_FILE}" | grep -F generated | awk '{print $1}')
 sudo chown -R $(id -un):$(id -gn) "${SCRIPT_DIR}/${DEB_FILE}"
 ls "${SCRIPT_DIR}/${DEB_FILE}"
--- .build/rpm/buildRpm.sh
+++ .build/rpm/buildRpm.sh
@@ -6,14 +6,17 @@ SCRIPT_DIR=$( cd "$( dirname "${SCRIPT}" )" >/dev/null 2>&1 && pwd )
 VERSION=$(git describe --tags HEAD)
 TAR_FILE=$("${SCRIPT_DIR}/../tar/buildTar.sh")
 
-mkdir -p ~/rpmbuild/SOURCES
-cp -a "${TAR_FILE}" ~/rpmbuild/SOURCES
+mkdir -p "$HOME/rpmbuild/SOURCES"
+cp -a "${TAR_FILE}" "$HOME/rpmbuild/SOURCES"
 
 cd ${SCRIPT_DIR} || exit 1
-{
-cat ChaosSquirrel.spec.in \
-| sed s/@VERSION@/$VERSION/g
-} > ChaosSquirrel.spec
+for TFILE in ChaosSquirrel.spec; do
+    cat "${TFILE}.in" \
+        | sed "s/@VERSION@/$VERSION/g" \
+        > "${TFILE}"
+done
+
 RPM_FILE=$(rpmbuild -bb ChaosSquirrel.spec 2>&1 | grep -F Wrote: | awk '{print $2}')
 [ -f "${RPM_FILE}" ] && mv "${RPM_FILE}" "${SCRIPT_DIR}"
+
 ls "${SCRIPT_DIR}/$(basename "${RPM_FILE}")"
