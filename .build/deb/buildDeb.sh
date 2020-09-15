59aa3dfede39ce5f0a2751e6b4617a5530fd557d
Karthik Kumar Viswanathan <karthikkumar@gmail.com>
2020-09-14 22:43:39 -0700

    Move to separate directories.

--- .build/deb/buildDeb.sh
+++ .build/deb/buildDeb.sh
@@ -3,5 +3,5 @@
 SCRIPT=$(realpath "${BASH_SOURCE[0]}")
 SCRIPT_DIR=$( cd "$( dirname "${SCRIPT}" )" >/dev/null 2>&1 && pwd )
 
-RPM_FILE=$("${SCRIPT_DIR}/buildRpm.sh")
+RPM_FILE=$("${SCRIPT_DIR}/../rpm/buildRpm.sh")
 sudo alien -v -d --scripts "${RPM_FILE}"
---
+++ .build/tar/buildTar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT=$(realpath "${BASH_SOURCE[0]}")
+SCRIPT_DIR=$( cd "$( dirname "${SCRIPT}" )" >/dev/null 2>&1 && pwd )
+
+VERSION=$(git describe --tags HEAD)
+ROOT=$(git rev-parse --show-toplevel)
+cd "${ROOT}" && git archive --format=tar HEAD --prefix="ChaosSquirrel-${VERSION}/" --output="${SCRIPT_DIR}/${VERSION}.tar.gz"
